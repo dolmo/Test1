acad14acc5a996a549af12943a2341fcbddca6cc
David Olmo <olmomarchal@gmail.com>
2018-01-20 20:08:35 -0500

    Update

---
+++ main.py
@@ -0,0 +1,5 @@
+#Here is my age input program
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2018 - age)+100)
+print(name + " will be 100 years old in the year " + year)
