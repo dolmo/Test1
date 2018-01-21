415b0a04e5da3ea6216958370faf9389aa697eef
David Olmo <olmomarchal@gmail.com>
2018-01-20 20:19:02 -0500

    Update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+
+#Hello this is my first raw input project.
+
 name = raw_input("What is your name: ")
 age = int(raw_input("How old are you: "))
 year = str((2014 - age)+100)
-print(name + " will be 100 years old in the yedaar " + year)
+print(name + " will be 100 years old in the year " + year)
