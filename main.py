07ee46cb415d8dac383c8a6f6633870b3702f68c
David Olmo <olmomarchal@gmail.com>
2018-01-20 20:20:48 -0500

    `New Comments`

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 #Hello this is my first raw input project.
-
+#another commit to change name
 name = raw_input("What is your name: ")
 age = int(raw_input("How old are you: "))
 year = str((2014 - age)+100)
